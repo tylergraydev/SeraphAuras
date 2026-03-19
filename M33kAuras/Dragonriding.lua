027edf9304dd9cf9cd2cf176a28113d0764f4f23
m33shoq <mishadyrov@gmail.com>
2026-03-20 01:13:15 +0200

    fix dragonriding tracking

--- M33kAuras/Dragonriding.lua
+++ M33kAuras/Dragonriding.lua
@@ -4,6 +4,8 @@ local AddonName = ...
 ---@class Private
 local Private = select(2, ...)
 
+local GetBonusBarIndex, GetBonusBarOffset = C_ActionBar.GetBonusBarIndex, C_ActionBar.GetBonusBarOffset
+
 local function HandleEvent(self, event, arg1)
   Private.callbacks:Fire("WA_DRAGONRIDING_UPDATE")
   if event == "PLAYER_ENTERING_WORLD" and arg1 == true then
@@ -14,10 +16,12 @@ end
 local frame = CreateFrame("Frame")
 frame:RegisterEvent("UNIT_POWER_BAR_SHOW")
 frame:RegisterEvent("UNIT_POWER_BAR_HIDE")
+frame:RegisterEvent('PLAYER_CAN_GLIDE_CHANGED')
+frame:RegisterEvent('PLAYER_IS_GLIDING_CHANGED')
 frame:RegisterEvent("PLAYER_ENTERING_WORLD")
 frame:SetScript("OnEvent", HandleEvent)
 
 Private.IsDragonriding = function ()
-  return UnitPowerBarID("player") == 631
+  return GetBonusBarIndex() == 11 and GetBonusBarOffset() == 5
 end
 
